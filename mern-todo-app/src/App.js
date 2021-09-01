9e180ae00797a81d8bd5de12bc3df10adc182308
John Le <tuanlle63@gmail.com>
2021-09-02 10:55:10 +1200

    MongoDB

--- mern-todo-app/src/App.js
+++ mern-todo-app/src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
       <Router>
         <div className="container">
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="https://codingthesmartway.com" target="_blank">
+            <a class="navbar-brand" href="https://codingthesmartway.com" >
               <img src={logo} width="30" height="30" alt="CodingTheSmartWay.com" />
             </a>
             <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
